26646a9653fe506be74239e4351ed246f70b3653
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-08 15:29:10 +0000

    index.php template

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="YouCentral is a unique YouTube partnership network connecting people & tools - towards success">
+		<meta name="keywords" content="YouCentral"/>
+		<meta name="google-site-verification" content=""/>
+		<meta name="author" content="YouCentral - YouTube Partnership Network">
+		<meta name="application-name" content="YouCentral"/>
+		<meta property="og:title" content='YouCentral - YouTube Partnership Network'/>
+		<meta property="og:type" content="article"/>
+		<meta property="og:image" content="./img/logo.png"/>
+		<meta property="og:description" content="YouCentral is a unique YouTube partnership network connecting people & tools - towards success"/>
+		<meta name="twitter:card" content="summary"/>
+		<meta name="twitter:title" content='YouCentral - YouTube Partnership Network'/>
+		<meta name="twitter:description" content="YouCentral is a unique YouTube partnership network connecting people & tools - towards success"/>
+		<meta name="twitter:image" content="./img/logo.png"/>
+		<title>
+			YouCentral - YouTube Partnership Network
+		</title>
+		<link media="all" type="text/css" rel="stylesheet" href="./css/normalize.css">
+		<link media="all" type="text/css" rel="stylesheet" href="./css/main.css">
+	</head>
+	<body>
+		<header>
+		</header>
+		<content>
+		</content>
+		<footer>
+		</footer>
+	</body>
+</html>
