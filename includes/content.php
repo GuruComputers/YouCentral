28991e4e863d3c60c593a2ef795ef24814c3977d
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-09 14:28:23 +0000

    More Content Work

--- includes/content.php
+++ includes/content.php
@@ -2,21 +2,21 @@
     <div class="row text-center">
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/youtube.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>No Lock in Contract/h3>
+        		<img src="../img/unlock.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<h3>No Lock in Contract</h3>
         		<p>We are a subnetwork of Freedom</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/unlock.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/eye.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>Transparency</h3>
         		<p>Come or go anytime, you decide when (just give us 30 days notice)</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/eye.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/revenue.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>Revenue Share</h3>
         		<p>We enable revenue visibility in your YouTube channel and nothing is hidden</p>
         	</p>
@@ -25,21 +25,21 @@
     <div class="row text-center">
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/revenue.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/money.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>No Minimum Payout</h3>
         		<p>60% base revenue share (As you grow, your percentage will get bigger!)</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/money.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/key.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>Ownership</h3>
         		<p>If you earn $1, we pay you $1</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/key.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/tick.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>Content ID</h3>
         		<p>You own your channel and content, and we are your technology partner</p>
         	</p>
@@ -48,21 +48,21 @@
     <div class="row text-center">
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/tick.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/music.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>Music</h3>
         		<p>We can find all copies of your original content anywhere on YouTube and monetize or takedown the matches (if you are a musician, this means we can monetize all copies of your music anywhere on YouTube!)</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/music.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/star.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>Record Labels</h3>
         		<p>See www.youtube.com/musiconanytv for the music you may use as our partner!</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
-        		<img src="../img/star.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+        		<img src="../img/dollar.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
         		<h3>No withholding tax</h3>
         		<p>Many record labels signed with us, and we negotiated access to professional music for you!</p>
         	</p>
