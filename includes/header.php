72f584bed382d225d2905d81923fe139e9e9a671
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-09 11:31:05 +0000

    Make Banner Responsive

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <div class="header">
 	<center>
-		<img src="img/banner.png" alt="main banner image" class="banner" usemap="#imgmap20151911926">
+		<img src="img/banner.png" alt="main banner image" class="img-responsive banner" usemap="#imgmap20151911926">
 		<map id="imgmap20151911926" name="imgmap20151911926">
 			<area shape="rect" alt="YouTube Link" title="Visit our YouTube Channel!" coords="201,200,346,241" href="http://www.youtube.com/user/youcentral" target="_blank" />
 			<area shape="rect" alt="Facebook Link" title="Visit our Facebook Page!" coords="200,243,344,280" href="http://www.facebook.com/youcentral" target="_blank" />
