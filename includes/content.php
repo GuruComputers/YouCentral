9e9c53ebe62311508784f1eec3f26c07c1a06a1c
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-09 12:14:35 +0000

    Add Grid System

--- includes/content.php
+++ includes/content.php
@@ -1,4 +1,19 @@
-<div class="row barone">
-	<div class="col-lg-2">
-	</div>
+<div class="container">
+    <div class="row Benefits">
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/" alt="">
+        	</p>
+        </div>
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/" alt="">
+        	</p>
+        </div>
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/" alt="">
+        	</p>
+        </div>
+    </div>
 </div>
