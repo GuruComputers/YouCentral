ff6c78bc430c9c64d9d4a8a06f961fd00d722e4e
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-09 14:03:54 +0000

    copyright slide to the right

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,7 @@
 <br /><br />
 <div class="footer_container">
     <div class="row">
-        <div class="col-sm-4"><?php echo "&copy"." ".$cy; ?></div>
+        <div class="col-sm-4 footer_copyright"><?php echo "&copy"." ".$cy; ?></div>
         <div class="col-sm-1"><a href=""><img src="img/facebook.png" alt="facebook icon" class="img-responsive footer_icon"></a></div>
         <div class="col-sm-1"><a href=""><img src="img/twitter.png" alt="twitter icon" class="img-responsive footer_icon"></a></div>
         <div class="col-sm-4">Free website developed by <a href="http://www.gurucomputers.co.uk">Guru Computers Ltd</a></div>
