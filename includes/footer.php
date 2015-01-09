ce8d7a76065dcb4fbabad1e0efb6a929c7fccfc3
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-09 13:46:41 +0000

    footer started

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<?php
+	$start=2015;
+	$current=date("Y");
+	$cy="";
+	if ($current==$start) {
+		$cy=$start;
+	}
+	if ($current>$start) {
+		$cy=$start."-".$current;
+	}
+?>
+<div class="footer_container">
+    <div class="row">
+        <div class="col-sm-3"><?php echo "&copy"." ".$cy; ?></div>
+        <div class="col-sm-3"><a href=""><img src="img/facebook.png" alt="facebook icon" class="img-responsive footer_icon"></a></div>
+        <div class="col-sm-3"><a href=""><img src="img/twitter.png" alt="twitter icon" class="img-responsive footer_icon"></a></div>
+        <div class="col-sm-3">Free website developed by <a href="http://www.gurucomputers.co.uk">Guru Computers Ltd</a></div>
+    </div>
+</div>
