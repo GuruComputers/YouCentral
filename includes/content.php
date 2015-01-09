67ef0a8ee65510bb6c8c83d535eb6c48cb17b7ed
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-09 14:25:45 +0000

    Correct Headings

--- includes/content.php
+++ includes/content.php
@@ -3,21 +3,21 @@
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/youtube.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>Networked with YouTube</h3>
+        		<h3>No Lock in Contract/h3>
         		<p>We are a subnetwork of Freedom</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/unlock.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>No Lock in Contract</h3>
+        		<h3>Transparency</h3>
         		<p>Come or go anytime, you decide when (just give us 30 days notice)</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/eye.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>Transparency</h3>
+        		<h3>Revenue Share</h3>
         		<p>We enable revenue visibility in your YouTube channel and nothing is hidden</p>
         	</p>
         </div>
@@ -26,21 +26,21 @@
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/revenue.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>Revenue Share</h3>
+        		<h3>No Minimum Payout</h3>
         		<p>60% base revenue share (As you grow, your percentage will get bigger!)</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/money.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>No Minimum Payout</h3>
+        		<h3>Ownership</h3>
         		<p>If you earn $1, we pay you $1</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/key.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>Ownership</h3>
+        		<h3>Content ID</h3>
         		<p>You own your channel and content, and we are your technology partner</p>
         	</p>
         </div>
@@ -49,21 +49,21 @@
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/tick.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>Content ID</h3>
+        		<h3>Music</h3>
         		<p>We can find all copies of your original content anywhere on YouTube and monetize or takedown the matches (if you are a musician, this means we can monetize all copies of your music anywhere on YouTube!)</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/music.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>Music</h3>
+        		<h3>Record Labels</h3>
         		<p>See www.youtube.com/musiconanytv for the music you may use as our partner!</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/star.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>Record Labels</h3>
+        		<h3>No withholding tax</h3>
         		<p>Many record labels signed with us, and we negotiated access to professional music for you!</p>
         	</p>
         </div>
@@ -72,21 +72,21 @@
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/dollar.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>No Withholding Tax</h3>
+        		<h3>Personal Mentors</h3>
         		<p>You earn 100% of everything you make. There are no tax forms to fill out from Freedom!, and no withholding tax. Simple.</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/business.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>We Sponsor Networks!</h3>
+        		<h3>Free Graphic Designers</h3>
         		<p>Tell us your business plan and why you want to be a network. If accepted, we are your technology partner but you make all the decisions.</p>
         	</p>
         </div>
         <div class="col-sm-4 benefit">
         	<p>
         		<img src="../img/business.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>We Sponsor Networks!</h3>
+        		<h3>Game Servers</h3>
         		<p>Tell us your business plan and why you want to be a network. If accepted, we are your technology partner but you make all the decisions.</p>
         	</p>
         </div>
@@ -95,7 +95,7 @@
      	<div class="col-sm-12 benefit">
      		<p>
      			<img src="../img/business.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<h3>We Sponsor Networks!</h3>
+        		<h3>Teamspeak</h3>
         		<p>Tell us your business plan and why you want to be a network. If accepted, we are your technology partner but you make all the decisions.</p>
      		</p>
      	</div>
