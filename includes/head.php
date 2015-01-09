b3c896a7d6a15b1e38c94652c62c82df8de4c7ca
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-09 10:03:53 +0000

    YouCentral is a unique YouTube Multi-Channel Network.

--- includes/head.php
+++ includes/head.php
@@ -4,18 +4,18 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="YouCentral is a unique YouTube partnership network connecting people & tools - towards success">
+		<meta name="description" content="YouCentral is a unique YouTube Multi-Channel Network.">
 		<meta name="keywords" content="YouCentral"/>
 		<meta name="google-site-verification" content=""/>
-		<meta name="author" content="YouCentral - YouTube Partnership Network">
+		<meta name="author" content="YouCentral is a unique YouTube Multi-Channel Network.">
 		<meta name="application-name" content="YouCentral"/>
-		<meta property="og:title" content='YouCentral - YouTube Partnership Network'/>
+		<meta property="og:title" content='YouCentral is a unique YouTube Multi-Channel Network.'/>
 		<meta property="og:type" content="article"/>
 		<meta property="og:image" content="./img/logo.png"/>
-		<meta property="og:description" content="YouCentral is a unique YouTube partnership network connecting people & tools - towards success"/>
+		<meta property="og:description" content="YouCentral is a unique YouTube Multi-Channel Network."/>
 		<meta name="twitter:card" content="summary"/>
-		<meta name="twitter:title" content='YouCentral - YouTube Partnership Network'/>
-		<meta name="twitter:description" content="YouCentral is a unique YouTube partnership network connecting people & tools - towards success"/>
+		<meta name="twitter:title" content='YouCentral is a unique YouTube Multi-Channel Network.'/>
+		<meta name="twitter:description" content="YouCentral is a unique YouTube Multi-Channel Network."/>
 		<meta name="twitter:image" content="./img/logo.png"/>
 		<title>
 			YouCentral - YouTube Partnership Network
