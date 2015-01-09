03188883c6b5e99ef780aad78d1d3eca22576c37
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-09 12:43:18 +0000

    More Content Work

--- includes/content.php
+++ includes/content.php
@@ -1,18 +1,65 @@
 <div class="container">
-    <div class="row Benefits">
+    <div class="row benefits1">
         <div class="col-sm-4">
         	<p>
-        		<img src="../img/" alt="">
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        		<p>We are a subnetwork of Freedom</p>
         	</p>
         </div>
         <div class="col-sm-4">
         	<p>
-        		<img src="../img/" alt="">
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
         	</p>
         </div>
         <div class="col-sm-4">
         	<p>
-        		<img src="../img/" alt="">
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+    </div>
+    <div class="row benefits2">
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+    </div>
+    <div class="row benefits3">
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+        <div class="col-sm-4">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+    </div>
+    <div class="row benefits4">
+        <div class="col-sm-6">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
+        	</p>
+        </div>
+        <div class="col-sm-6">
+        	<p>
+        		<img src="../img/youtube.png" alt="Direct with YouTube" class="benefit_icon">
         	</p>
         </div>
     </div>
