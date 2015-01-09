e45f3d954ea25252a05a65d6ee7a4928258cf652
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-09 13:00:08 +0000

    CSS Tweaks

--- includes/content.php
+++ includes/content.php
@@ -1,66 +1,68 @@
-<div class="benefits">
-    <div class="row benefits1">
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/youtube.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        		<p>We are a subnetwork of Freedom</p>
-        	</p>
-        </div>
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/unlock.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/eye.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-    </div>
-    <div class="row benefits2">
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/revenue.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/money.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/key.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-    </div>
-    <div class="row benefits3">
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/tick.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/music.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-        <div class="col-sm-4">
-        	<p>
-        		<img src="../img/star.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-    </div>
-    <div class="row benefits4">
-        <div class="col-sm-6">
-        	<p>
-        		<img src="../img/dollar.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-        <div class="col-sm-6">
-        	<p>
-        		<img src="../img/business.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
-        	</p>
-        </div>
-    </div>
-</div>
+<center>
+	<div class="benefits">
+	    <div class="row benefits1">
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/youtube.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        		<p>We are a subnetwork of Freedom</p>
+	        	</p>
+	        </div>
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/unlock.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/eye.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	    </div>
+	    <div class="row benefits2">
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/revenue.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/money.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/key.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	    </div>
+	    <div class="row benefits3">
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/tick.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/music.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	        <div class="col-sm-4">
+	        	<p>
+	        		<img src="../img/star.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	    </div>
+	    <div class="row benefits4">
+	        <div class="col-sm-6">
+	        	<p>
+	        		<img src="../img/dollar.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	        <div class="col-sm-6">
+	        	<p>
+	        		<img src="../img/business.png" alt="Direct with YouTube" class="img-responsive benefit_icon">
+	        	</p>
+	        </div>
+	    </div>
+	</div>
+</center>
