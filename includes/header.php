d5d0ad87d111446220bbc89d0f3def7b6f53ee42
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-09 11:04:07 +0000

    header defined 1st run

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<div class="header">
+	<center>
+		<img src="img/banner.png" alt="main banner image" class="banner">
+		<br />
+		<p class="mcn">A Youtube MCN</p>
+		<br />
+		<p class="partner">Partner<img src="img/partner.png" alt="hands shaking to signify partner" class="partner_img"></p>
+		<br />
+	</center>
+</div>
