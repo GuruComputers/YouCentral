ea0174e548284ad71589342523f94c807f8da438
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-09 13:54:01 +0000

    3 to 1 & 3 to 4

--- includes/footer.php
+++ includes/footer.php
@@ -11,9 +11,9 @@
 ?>
 <div class="footer_container">
     <div class="row">
-        <div class="col-sm-3"><?php echo "&copy"." ".$cy; ?></div>
-        <div class="col-sm-3"><a href=""><img src="img/facebook.png" alt="facebook icon" class="img-responsive footer_icon"></a></div>
-        <div class="col-sm-3"><a href=""><img src="img/twitter.png" alt="twitter icon" class="img-responsive footer_icon"></a></div>
-        <div class="col-sm-3">Free website developed by <a href="http://www.gurucomputers.co.uk">Guru Computers Ltd</a></div>
+        <div class="col-sm-4"><?php echo "&copy"." ".$cy; ?></div>
+        <div class="col-sm-1"><a href=""><img src="img/facebook.png" alt="facebook icon" class="img-responsive footer_icon"></a></div>
+        <div class="col-sm-1"><a href=""><img src="img/twitter.png" alt="twitter icon" class="img-responsive footer_icon"></a></div>
+        <div class="col-sm-4">Free website developed by <a href="http://www.gurucomputers.co.uk">Guru Computers Ltd</a></div>
     </div>
 </div>
