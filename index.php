ed6885b5afc478433c1c5030f90b74066369f0f6
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-09 10:15:53 +0000

    Define Defines

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
 	<script src="//code.jquery.com/jquery.js"></script>
 	<script src="bootstrap/dist/js/bootstrap.min.js"></script>
 		<header>
+			<?php
+				include 'includes/header.php';
+			?>
 		</header>
 		<content>
+			<?php
+				include 'includes/content.php';
+			?>
 		</content>
 		<footer>
+			<?php
+				include 'includes/footer.php';
+			?>
 		</footer>
 	</body>
 </html>
