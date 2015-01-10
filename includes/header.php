c3d4f7b89a0a358d86ac11bf08f65198f4d019f5
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-10 13:24:46 +0000

    Header Update to Remove Alert

--- includes/header.php
+++ includes/header.php
@@ -11,9 +11,6 @@
 	<script>
 		$(document).ready(function(e) {
 			$('img[usemap]').rwdImageMaps();
-			$('area').on('click', function() {
-				alert($(this).attr('alt') + ' clicked');
-			});
 		});
 	</script>
 	<div class="mcn_container">
