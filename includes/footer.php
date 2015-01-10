07c2eebf1c4edbbddf88e04ed3dcb3becf7f28ec
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-10 13:07:25 +0000

    footer tweak

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,7 @@
 <br /><br />
 <div class="footer_container">
     <div class="row">
-        <div class="col-sm-5 footer_copyright"><?php echo "&copy;"." <img href='img/logo.png' class='img-responsive footer_icon' YouCentral ".$cy; ?></div>
+        <div class="col-sm-5 footer_copyright"><?php echo '&copy; <img href="img/logo.png" class="img-responsive footer_icon"> YouCentral '.$cy; ?></div>
         <div class="col-sm-1"><a href="http://www.facebook.com/youcentral"  target="_blank"><img src="img/facebook.png" alt="facebook icon" class="img-responsive footer_icon"></a></div>
         <div class="col-sm-1"><a href="http://www.twitter.com/youcentral"  target="_blank"><img src="img/twitter.png" alt="twitter icon" class="img-responsive footer_icon"></a></div>
         <div class="col-sm-5 footer_guru">Free website developed by <a href="http://www.gurucomputers.co.uk">Guru Computers Ltd</a></div>
