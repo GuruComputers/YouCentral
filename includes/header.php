c26cefc727b42a29ad6cfb2652150cbbb464811c
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-11 22:50:09 +0000

    Update Social Links

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,10 @@
 <div class="header">
 	<img src="img/banner.png" alt="main banner image" class="img-responsive banner" usemap="#imgmap20151911926">
 	<map id="imgmap20151911926" name="imgmap20151911926">
-		<area shape="rect" alt="YouTube Link" title="Visit our YouTube Channel!" coords="201,200,346,241" href="http://www.youtube.com/user/youcentral" target="_blank" />
-		<area shape="rect" alt="Facebook Link" title="Visit our Facebook Page!" coords="200,243,344,280" href="http://www.facebook.com/youcentral" target="_blank" />
+		<area shape="rect" alt="YouTube Link" title="Visit our YouTube Channel!" coords="201,200,346,241" href="https://www.youtube.com/user/youcentralmcn" target="_blank" />
+		<area shape="rect" alt="Facebook Link" title="Visit our Facebook Page!" coords="200,243,344,280" href="https://www.facebook.com/pages/YouCentral/407878222694691?ref=bookmarks" target="_blank" />
 		<area shape="rect" alt="Twitter Link" title="Visit our Twitter Page!" coords="202,287,343,318" href="http://www.twitter.com/youcentral" target="_blank" />
-		<area shape="rect" alt="Alternative YouTube Link" title="Visit our YouTube Channel!" coords="443,328,874,380" href="http://www.youtube.com/user/youcentral" target="_blank" />
+		<area shape="rect" alt="Alternative YouTube Link" title="Visit our YouTube Channel!" coords="443,328,874,380" href="https://www.youtube.com/user/youcentralmcn" target="_blank" />
 	</map>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="../js/jquery.rwdImageMaps.min.js"></script>
