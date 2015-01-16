928aecfeddb2eb6e9cf68763454c0cd556ffc60d
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-16 09:01:28 +0000

    Update Facebook Footer Link

--- includes/footer.php
+++ includes/footer.php
@@ -13,7 +13,7 @@
 <div class="footer_container">
     <div class="row">
         <div class="col-sm-5 footer_copyright"><?php echo '&copy; YouCentral '.$cy; ?></div>
-        <div class="col-sm-1"><a href="http://www.facebook.com/youcentral"  target="_blank"><img src="img/facebook.png" alt="facebook icon" class="img-responsive footer_icon"></a></div>
+        <div class="col-sm-1"><a href="facebook.com/pages/YouCentral/407878222694691?ref=bookmarks"  target="_blank"><img src="img/facebook.png" alt="facebook icon" class="img-responsive footer_icon"></a></div>
         <div class="col-sm-1"><a href="http://www.twitter.com/youcentral"  target="_blank"><img src="img/twitter.png" alt="twitter icon" class="img-responsive footer_icon"></a></div>
         <div class="col-sm-5 footer_guru">Free website developed by <a href="http://www.gurucomputers.co.uk">Guru Computers Ltd</a></div>
     </div>
